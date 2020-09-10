d7b4c2cf47c789e2ad334234a100812c056123b8
张啟财 <zhangqicai@vizvision.com>
2020-09-10 10:17:24 +0000

    feat (通过udp实现protobuf使用场景python demo)

--- common/demo_udp_server.py
+++ common/demo_udp_server.py
@@ -6,11 +6,33 @@ import socket
 import time
 
 
+def gen_person_info(people:list, person_id:int, name='test', email="test@viz.com"):
+    person = pb2.Person()
+    person.id = person_id
+    person.name = name
+    person.email = email
+
+    phone_number = person.phones.add()
+    phone_number.number = "2222800"
+    phone_number.type = pb2.Person.WORK
+
+    people.append(person)
+
+
 if __name__ == '__main__':
+    demo_info =  pb2.demo_info()
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    
+    count = 0
     while True:
-        print("server is running")
-        buf = "test"
-        sock.sendto(buf.encode(), ('255.255.255.255',9999))
+        print("server is running ", count)
+        _id = count
+        _name = f"zh{count}"
+        _email = f"{count}@viz.com" 
+        demo_info.Clear()
+        gen_person_info(demo_info.people, _id, _name, _email)
+        buf = demo_info.SerializeToString()
+        sock.sendto(buf, ('255.255.255.255',9999))
         time.sleep(1)
+        count += 1
--- python/demo_udp_client.py
+++ python/demo_udp_client.py
@@ -4,7 +4,23 @@ import demo_pb2 as pb2
 import socket
 import time
 
+def show_info(people):
+    for person in people:
+        print("id: ", person.id)
+        print("name: ", person.name)
+        print("email: ", person.email)
+        for phone_number in person.phones:
+            if phone_number.type == pb2.Person.MOBILE:
+                print(" Mobile phone: ", end=" ")
+            if phone_number.type == pb2.Person.HOME:
+                print(" Home phone: ", end=" ")
+            if phone_number.type == pb2.Person.WORK:
+                print(" Work phone: ", end=" ")
+            print(phone_number.number)
+
+
 if __name__ == "__main__":
+    demo_info = pb2.demo_info()
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     client.bind(('', 9999))
@@ -12,7 +28,9 @@ if __name__ == "__main__":
     cnt = 0
     while True:
         recv_data = client.recv(1024)
-        print(recv_data)
+        demo_info.ParseFromString(recv_data)
+        print("unparse data :",recv_data)
+        show_info(demo_info.people)
         time.sleep(1)
         cnt += 1
         if cnt > 10:
